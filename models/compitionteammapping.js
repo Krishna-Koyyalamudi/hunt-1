de5ad4dfc00e3b7abe95d59a7ddb8faf0857a91b
pruthvi-naskanti <60023780+pruthvi-naskanti@users.noreply.github.com>
2020-11-18 01:56:23 -0600

    Update compitionteammapping.js

--- models/compitionteammapping.js
+++ models/compitionteammapping.js
@@ -1,1 +1,12 @@
-
+const mongoose = require("mongoose")
+const competitionteammappingSchema = new mongoose.Schema({
+    competitionId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        unique:true,
+    },
+    competitionId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        unique:true,
+    },
