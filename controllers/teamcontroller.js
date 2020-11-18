baac304ba7d59bf4599849ed4f1ed8a1f0e135e1
GD-Prasad <g.d.prasad004@gmail.com>
2020-11-18 00:36:36 -0600

    Added deleted function to the teamcontroller.js

--- controllers/teamcontroller.js
+++ controllers/teamcontroller.js
@@ -4,29 +4,29 @@ const app = express();
 const Team = require('../models/team');
 var exports = module.exports = {};
 
-exports.teamlist = function (req, res) {
+exports.teamlist = function(req, res) {
     res.render('team/index');
 }
 
 // exports.teamlist = function(req, res){
 //     teams.find({}, function (err, ) {
-        
+
 //     })
 // }
 
-exports.createteam = function (req, res) {
+exports.createteam = function(req, res) {
     res.render('team/create');
 }
 
-exports.tinvite = function (req, res) {
+exports.tinvite = function(req, res) {
     res.render('team/teamInvite');
 }
 
-exports.pinvite = function (req, res) {
+exports.pinvite = function(req, res) {
     res.render('team/invitePlayers');
 }
 
-exports.addTeam = function (req, res) {
+exports.addTeam = function(req, res) {
 
     // Get our form values. These rely on the "name" attributes
     var teamId = req.body.teamid;
@@ -40,13 +40,12 @@ exports.addTeam = function (req, res) {
 
     const newTeam = new Team(teamdata);
 
-    newTeam.save(function (err) {
+    newTeam.save(function(err) {
         if (err) {
             console.log(err)
-            // If it failed, return error
+                // If it failed, return error
             res.send("There was a problem adding the information to the database.");
-        }
-        else {
+        } else {
             // And forward to success page
             console.log("Data Saved");
             res.redirect("/team/index");
@@ -54,8 +53,35 @@ exports.addTeam = function (req, res) {
     });
 };
 
+exports.deleteTeam = function(req, res) {
+
+    // Get our form values. These rely on the "name" attributes
+    var teamId = req.body.teamid;
+    var teamName = req.body.teamname;
+
+    // Submit to the DB
+    const teamdata = {
+        teamid: teamId,
+        teamname: teamName
+    };
+
+    const newTeam = new Team(teamdata);
+
+    newTeam.remove(function(err) {
+        if (err) {
+            console.log(err)
+                // If it failed, return error
+            res.send("There was a problem deleting the information from the database.");
+        } else {
+            // And forward to success page
+            console.log("Data Deleted");
+            res.redirect("/team/index");
+        }
+    });
+};
+
 /* GET Teamlist page. */
-exports.teamlist = async function (req, res) {
+exports.teamlist = async function(req, res) {
     try {
         const teams = await Team.find()
         console.log(teams)
