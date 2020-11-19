05d19145f4fe42e43760d179330ad7f8dfe50a70
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2020-11-18 20:43:16 -0600

    Updated questcontroller.js

--- controllers/questcontroller.js
+++ controllers/questcontroller.js
@@ -4,22 +4,32 @@ const app = express();
 const Quest = require('../models/quest');
 var exports = module.exports = {};
 
+// Render view to add a new quest
 exports.createquest = function (req, res) {
     res.render('quest/create');
 }
 
-exports.questlist = function (req, res) {
-    res.render('quest/index');
-}
-
+// api to CREATE a new quest data
 exports.addquest = function (req, res) {
 
 }
 
-exports.removequest = function (req, res) {
+// Render view with list of quests
+exports.questlist = function (req, res) {
+    res.render('quest/index');
+}
+
+// api to GET list of quets data
+exports.getquest = function (req, res) {
 
 }
 
+// api to UPDATE quest data
 exports.editquest = function (req, res) {
+
+}
+
+// api to DELETE a quest data
+exports.removequest = function (req, res) {
 
 }
