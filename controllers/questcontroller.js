205b4ce089b4b8de8b9b6248f199b8bae35c39cb
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2020-11-18 20:30:03 -0600

    update teamcontroller.js

--- controllers/questcontroller.js
+++ controllers/questcontroller.js
@@ -4,10 +4,22 @@ const app = express();
 const Quest = require('../models/quest');
 var exports = module.exports = {};
 
-exports.createquest = function(req, res) {
+exports.createquest = function (req, res) {
     res.render('quest/create');
 }
 
-exports.questlist = function(req, res){
+exports.questlist = function (req, res) {
     res.render('quest/index');
+}
+
+exports.addquest = function (req, res) {
+
+}
+
+exports.removequest = function (req, res) {
+
+}
+
+exports.editquest = function (req, res) {
+
 }
