53ae00505b282e6dde50a594ae672afa35937153
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2021-01-10 09:36:56 -0600

    Separate layouts for login and home

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/css',express.static(__dirname+'public/css'))
 // view engine setup
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
+app.set('layout', 'layouts')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
