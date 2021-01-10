7b0aa353208c7c6b63b9472ff51190950740dd26
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2021-01-10 01:03:38 -0600

    Rescaffolding to fix ejs

---
+++ server.js
@@ -0,0 +1,37 @@
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').parse()
+// }
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const dotenv = require('dotenv')
+dotenv.config({ path: '.env' })
+
+const indexRouter = require('./routes/index')
+
+// view engine setup
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+// Mongoose setup
+const mongoose = require('mongoose');
+const dev_db_url = process.env.ATLAS_URI;
+const mongoDB = dev_db_url || process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.Promise = global.Promise;
+const db = mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected !');
+    //seeder(db);
+});
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
